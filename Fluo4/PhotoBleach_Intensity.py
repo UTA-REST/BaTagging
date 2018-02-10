fece72afbb9ccdeffceb4fff04a54d8e8f1c4c7d
Fernanda Psihas <psihas@fnal.gov>
2018-02-10 02:06:04 -0600

    Added plots. Need to fix axis spacing.

--- Fluo4/PhotoBleach_Intensity.py
+++ Fluo4/PhotoBleach_Intensity.py
@@ -21,9 +21,9 @@ import os
 import glob
 import pandas as pd
 import pylab
-from scipy.optimize import curve_fit
-from scipy.signal import savgol_filter
-from scipy import integrate
+# from scipy.optimize import curve_fit
+# from scipy.signal import savgol_filter
+# from scipy import integrate
 
 # Non-hobo style
 # <codecell>
@@ -54,79 +54,59 @@ S_20E    = []
 S_30E    = []
 S_30E30L = []
 
-FileID = ["/control-youmustlearncontrol/", "FC*E0min", "FC*E5min", "FC*E10min", "FC*E20min", "FC*E30min", "FC*E30minD30"]
-Names  = ["Control", "O min Exp", "5 min Exp", "10 min Exp", "20 min Exp", "30 min Exp", "30 min Exp + 30 min"]
-Types  = [S_control,S_0E ,S_5E ,S_10E ,S_20E ,S_30E, S_30E30L]
+FileID = ["FC*E0min", "FC*E5min", "FC*E10min", "FC*E20min", "FC*E30min", "FC*E30minD30","contol*/"]
+Names  = ["O min Exp", "5 min Exp", "10 min Exp", "20 min Exp", "30 min Exp", "30 min Exp + 30 min","Control"]
+Types  = [S_0E ,S_5E ,S_10E ,S_20E ,S_30E, S_30E30L,S_control]
+# <codecell>
 
-Curves=[]
 for iCurve,Curves in enumerate(Types):
+    print(iCurve)
+
+for iCurve,Curves in enumerate(Types):
+    Curve=[]
     for files in glob.glob(Path + FileID[iCurve] + '*.csv'):
-        Curves.append(files)
+        Curve.append(files)
+    print(Curve)
     print(Names[iCurve],Curves)
+    # last one sees too many files  ¯\_(ツ)_/¯
 
-# last one sees too many files  ¯\_(ツ)_/¯
+
+    DATA = dict()
+    for x in range(0,len(Curve)):
+        print(Curve[x])
+        DATA[x] = pd.read_csv(Curve[x],delimiter=',',names=['Wavelength','Intensity','nah'])
+    DATA[1]['Wavelength'][3:30]
+    DATA[1]['Intensity'][3:30]
+
+    MyX=DATA[1]['Wavelength'][3:30]
+    MyY=DATA[1]['Intensity'][3:30]
+
+    print(" ---- ")
+    print(np.array(MyX))
+    print(np.array(MyY))
+
+    #
+    # fig, ax = plt.subplots()
+    # binX = np.linspace(500,550,50)
+    # binY = np.linspace(400,450,50)
+    # ax.scatter(np.array(MyX),np.array(MyY),binX,binY)
+    #
+    # ax.grid()
+
+    plt.show()
+    pylab.figure(figsize=(5,5))
+    pylab.grid()
+    pylab.plot(MyX,MyY,linewidth=2)
+    pylab.xlabel("W")
+    pylab.ylabel("I")
+
+    plt.figure(figsize=(10,10))
+
+    plt.plot(  np.array(MyX),np.array(MyY),color=cm.gist_heat((0+1)/7),label= r'$460 \mu M$'+' $Ba^{++}$',linewidth=2)
+
+    plt.xlabel(r'Wavelength / nm', fontsize=28)
+    plt.ylabel('Intensity (arb. units)', fontsize=28)
+    plt.show()
+
 
 # <codecell>
-
-
-for files in glob.glob(Path + '*.csv'):
-    Curves.append(files)
-print(Curves)
-DATA = dict()
-
-for x in range(0,len(files)):
-    DATA[x] = pd.read_csv(Sample[x],delimiter=',',names=['Wavelength','Intensity','nah'])
-DATA[0]['Wavelength'][0:11]
-#os.listdir()
-
-# <codecell>
-
-print(" ")
-# <codecell>
-
-
-
-######################################################
-# # I stil think this should work fine if I spend a bit of time thinking about it / figuring out how in the world one loops over columns
-# ######################################################
-# # Import pandas and excel thinger
-# import pandas as pd
-# from openpyxl import load_workbook
-#
-#  # <codecell>
-#
-# # Load in the workbook
-# wb = load_workbook('../DATA/FC_AllBa.xlsx')
-# # Get a sheet by name
-# sheet = wb.get_sheet_by_name('FC_AllBa')
-#
-#  # <codecell>
-# # Retrieve the value of a certain cell
-# sheet['A1'].value
-#
-# # Select element 'B2' of your sheet
-# c = sheet['B2']
-# v = sheet['B2'].value
-# # Retrieve the row number of your element
-# c.row
-# # Retrieve the column letter of your element
-# c.column
-# # Retrieve the coordinates of the cell
-# c.coordinate
-# v
-#
-#  # <codecell>
-#
-# #FC1E0minC=[]
-# DATA= dict()
-# for x in range(0,len(sheet.max_column))
-#     for cellObj in sheet['B3':'B99']:
-#         holder=[]
-#         for cell in cellObj:
-#             #FC1E0minC.append(cell.value)
-#             holder.append(cell.value)
-#         DATA
-#
-# # print (FC1E0minC)
-#
-#  # <codecell>
